f436f20f05ef301c41e773ffa2c8b5c682c32625
Pierre Marais <pierremarais67@gmail.com>
2022-12-13 16:06:04 +0100

    Extract into functions

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,44 @@ use flate2::write::ZlibEncoder;
 use flate2::read::ZlibDecoder;
 use std::fs::File;
 use std::io::Read;
+use std::str;
 
 fn main() {
     println!("hello there!");
     // Open and reads file to buffer
-    let file_name = "test.txt";
-    let mut file = File::open(file_name).expect("Unable to open file");
-    let metadata = fs::metadata(&file_name).expect("unable to read metadata");
-    let mut buffer = vec![0; metadata.len() as usize];
-    file.read(&mut buffer).expect("buffer overflow");
+    let file_name = String::from("test.txt");
+    let buffer = read_file(&file_name);
 
     // Compresses buffer
-    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-    e.write_all(&buffer);
-    let compressed_bytes = e.finish().expect("unable to compress bytes");
+    let compressed_bytes = compress_buffer(&buffer);
 
     // Decompresses buffer
-    let mut d = ZlibDecoder::new(&*compressed_bytes);
-    let mut s = String::new();
-    d.read_to_string(&mut s).unwrap();
+    let decompressed_bytes = decompress_buffer(compressed_bytes);
+
+    let file_contents = str::from_utf8(&decompressed_bytes).expect("Expected file contents to be string");
+
+    println!("{}", file_contents);
+}
+
+fn read_file(file_name: &String) -> Vec<u8> {
+    let mut file = File::open(file_name).expect("Unable to open file");
+    let metadata = fs::metadata(file_name).expect("unable to read metadata");
     let mut buffer = vec![0; metadata.len() as usize];
+    file.read(&mut buffer).expect("buffer overflow");
+    return buffer;
+}
+
+fn compress_buffer(buffer: &Vec<u8>) -> Vec<u8> {
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write_all(buffer).expect("Could not compress buffer");
+    return e.finish().expect("unable to compress bytes");
+}
+
+fn decompress_buffer(buffer: Vec<u8>) -> Vec<u8> {
+    let mut d = ZlibDecoder::new(&*buffer);
+    // let mut s = String::new();
+    // d.read_to_string(&mut s).unwrap();
+    let mut buffer = vec![0; buffer.len() as usize];
     d.read(&mut buffer).expect("unable to read back");
-    println!("File contents: {}", s);
+    return buffer
 }
