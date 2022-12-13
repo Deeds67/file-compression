c509a295e9cdcc074119a8dde459e4be5024f812
Pierre Marais <pierremarais67@gmail.com>
2022-12-13 14:50:30 +0100

    Read file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@
 // mod orderbook;
 // mod xmlparser;
 // mod order;
+use std::fs;
+
 fn main() {
     println!("hello there!");
+    let data = fs::read_to_string("test.txt").expect("Unable to read file");
+    println!("{}", data);
 }
