af435878809062984b0c2f028d40e9154edf8fde
Pierre Marais <pierremarais67@gmail.com>
2022-12-13 15:54:20 +0100

    Compress and decompress file contents

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,30 @@
 // mod order;
 use std::fs;
 
+use std::io::prelude::*;
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+use flate2::read::ZlibDecoder;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
     println!("hello there!");
-    let data = fs::read_to_string("test.txt").expect("Unable to read file");
-    println!("{}", data);
+    // Open and reads file to buffer
+    let file_name = "test.txt";
+    let mut file = File::open(file_name).expect("Unable to open file");
+    let metadata = fs::metadata(&file_name).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    file.read(&mut buffer).expect("buffer overflow");
+
+    // Compresses buffer
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    e.write_all(&buffer);
+    let compressed_bytes = e.finish().expect("unable to compress bytes");
+
+    // Decompresses buffer
+    let mut d = ZlibDecoder::new(&*compressed_bytes);
+    let mut s = String::new();
+    d.read_to_string(&mut s).unwrap();
+    println!("File contents: {}", s);
 }
